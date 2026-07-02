a3a298b79f7ab71cc2a51401d75e4d82ee8ee5a4
Sunny Kumar <sunnyw3c@gmail.com>
2026-07-02 12:34:24 +0530

    update css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@
   </script>
 
   <!-- Custom CSS Enhancements -->
-  <link rel="stylesheet" href="{{ asset('css/styles.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/styles.css') }}?v={{ time() }}" />
   @stack('head')
   @stack('styles')
 </head>
